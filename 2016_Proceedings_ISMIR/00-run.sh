7e88af63a514e4fafc3dac815f50122153f258a5
Michael Mandel <mim@mr-pc.org>
2016-07-01 22:22:56 -0400

    fixed compilation issues, switched to smaller times font for headers, footers, TOC, removed underscores from csv

--- 2016_Proceedings_ISMIR/00-run.sh
+++ 2016_Proceedings_ISMIR/00-run.sh
@@ -1,11 +1,10 @@
-#!/bin/bash -xuev
+#!/bin/bash
 
-rm -f 2016_Proceedings_ISMIR.aux
-rm -f 2016_Proceedings_ISMIR.ain
+rm -f 2016_Proceedings_ISMIR.{aux,ain,log,toc}
 pdflatex 2016_Proceedings_ISMIR.tex
 
 #ReplaceText.vbs 2016_Proceedings_ISMIR.aux "\IeC " ""
-sed -e 's/\IeC //g' 2016_Proceedings_ISMIR.aux > 2016_Proceedings_ISMIR_2.aux
+sed -e 's/\\IeC //g' 2016_Proceedings_ISMIR.aux > 2016_Proceedings_ISMIR_2.aux
 
 authorindex -d 2016_Proceedings_ISMIR_2.aux
 
