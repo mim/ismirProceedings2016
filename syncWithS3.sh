e2b72b09fb47a6710f3ea6f920f8c1d6f65b3052
Michael Mandel <mim@mr-pc.org>
2016-07-15 00:40:36 -0400

    script to sync to s3, line break in index

---
+++ syncWithS3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xue
+
+# Sync files with amazon s3 bucket using aws command line tool
+
+DEST=m.mr-pc.org/ismir16
+SRC=2016_Proceedings_ISMIR_Electronic/
+
+aws s3 sync $SRC s3://$DEST --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
