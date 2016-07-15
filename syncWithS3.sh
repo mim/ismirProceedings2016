d73567d5e62cdc14cbfd1f50bfc2bc5e02fea735
Michael Mandel <mim@mr-pc.org>
2016-07-15 13:05:05 -0400

    switched to new cover from Tia

--- syncWithS3.sh
+++ syncWithS3.sh
@@ -3,6 +3,8 @@
 # Sync files with amazon s3 bucket using aws command line tool
 
 DEST=m.mr-pc.org/ismir16
-SRC=2016_Proceedings_ISMIR_Electronic/
+SRC=2016_Proceedings_ISMIR_Electronic
 
-aws s3 sync $SRC s3://$DEST --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+mv $SRC.zip $SRC
+aws s3 sync $SRC/ s3://$DEST --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+mv $SRC/$SRC.zip .
